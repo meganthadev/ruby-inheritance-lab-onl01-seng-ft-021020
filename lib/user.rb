aebdf64c7f5843ee2ec89537e74d8493a1cfe79b
Megan Horsfield <meganthadev@gmail.com>
2020-03-03 00:03:28 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,10 @@
 class User 
   
-  attr_accessor :teacher, :student, 
+  attr_accessor :first_name, :last_name
+  
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end  
   
 end   
